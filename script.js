d12d6402d76115c83ec7c976a114e59ba0f45dec
stasssssi <121359980+stasssssi@users.noreply.github.com>
2023-11-08 17:27:45 +0000

    work

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 // задача №1
 const fill = (arraySize, value) => {
-  // решение здесь
+  const newArray = [];
+  for (i = 0; i < arraySize; i += 1) {
+    newArray.push(value);
+  }
+  return newArray;
 }
 
 const data = 3;
@@ -9,7 +13,11 @@ console.log(fill(data, valueToFill))
 
 // задача №2
 const reverse = (array) => {
-  // решение здесь
+  const newArray = [];
+  for (let i = array.length - 1; i >= 0; i -= 1) {
+    newArray.push(array[i]);
+  }
+  return newArray;
  }
 
 const data = [1, 2, 3];
@@ -17,7 +25,13 @@ console.log(reverse(data)); // [3, 2, 1]
 
 // задача №3
 const compact = (array) => {
-  // решение здесь
+  const result = [];
+  for (const item of array) {
+    if (item !== null && item !== false && item !== 0 && item !== undefined && item !== '') {
+    result.push(item);
+    }
+  }
+  return result;
 }
 
 const data = [0, 1, false, 2, undefined, '', 3, null];
